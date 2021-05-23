66ba8f4a1f614d23e467c3c7d99fdeac16dae241
dekunleroy <adebanjosegun589@gmail.com>
2021-05-23 05:50:04 +0100

    implement navigation

---
+++ src/navigation/stack/UserStack.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Users from '../../screens/users/Users';
+import Initial from '../../screens/users/Initial';
+
+const Stack = createStackNavigator();
+
+const MainStack = () => (
+  <Stack.Navigator initialRouteName="Initial">
+    <Stack.Screen
+      name="Users"
+      component={Users}
+      options={{
+        headerTitleAlign: 'center',
+        headerStyle: { backgroundColor: '#F1F1F1' },
+        headerLeft: () => (
+          null
+        ),
+      }}
+    />
+    <Stack.Screen
+      name="Initial"
+      component={Initial}
+      options={{
+        headerShown: false,
+      }}
+    />
+  </Stack.Navigator>
+);
+export default MainStack;
