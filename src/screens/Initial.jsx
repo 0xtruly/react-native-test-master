830a41d1fb0d977ae9a25f6c697cd40c542c44a3
dekunleroy <adebanjosegun589@gmail.com>
2021-05-24 11:22:10 +0100

    add initial navigation stack

---
+++ src/screens/Initial.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Loader from '../../components/Loader';
+
+const Initial = ({ navigation }) => {
+  const [loaderVisible, setLoaderVisible] = useState(true);
+
+  const dismissLoader = () => {
+    setTimeout(() => {
+      setLoaderVisible(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    dismissLoader();
+  }, []);
+
+  if (!loaderVisible) {
+    navigation.navigate('Users');
+  }
+  return <Loader />;
+};
+
+Initial.propTypes = {
+  navigation: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.func,
+  ]).isRequired,
+};
+export default Initial;
